3bd8fa647eeaf3b16cf42744ab637c7a4cbcfeae
yasirklc <73668281+yasirklc@users.noreply.github.com>
2020-10-30 17:01:15 +0300

    2 ekledim

--- ilkyukleme/ilkyukleme/Program.cs
+++ ilkyukleme/ilkyukleme/Program.cs
@@ -6,7 +6,7 @@ namespace ilkyukleme
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
         }
     }
 }
